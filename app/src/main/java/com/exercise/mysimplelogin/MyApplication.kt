bb9836109b167d04153547dcf1095f50d51176bb
Sitifatimah121 <sitifatimah.ft.13@gmail.com>
2023-01-13 00:02:32 +0700

    create MyApplication

--- app/src/main/java/com/exercise/mysimplelogin/MyApplication.kt
+++ app/src/main/java/com/exercise/mysimplelogin/MyApplication.kt
@@ -7,6 +7,7 @@ import org.koin.core.context.startKoin
 open class MyApplication : Application() {
     override fun onCreate() {
         super.onCreate()
+
         startKoin{
             androidContext(this@MyApplication)
             modules(storageModule)
